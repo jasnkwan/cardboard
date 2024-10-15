b2841ae1e0605c90e90c7d8edb3bc7963e7cee3c
Jason Kwan <jasnkwan@gmail.com>
2024-10-14 20:52:12 -0700

    Remove commented out lines in vite.config.js.

--- cardboard_ui/vite.config.js
+++ cardboard_ui/vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(
-    {
-      // Disable React Strict Mode in development
-      //reactStrictMode: false,
-    }
-  )],
+  plugins: [react()],
   server: {
     host: "127.0.0.1",
     proxy: {
@@ -19,9 +14,6 @@ export default defineConfig({
       }
     }
   },
-  //build: {
-  //  outDir: '../cardboard/resources'
-  //},
   resolve: {
     alias: {
       '@': '/src',
